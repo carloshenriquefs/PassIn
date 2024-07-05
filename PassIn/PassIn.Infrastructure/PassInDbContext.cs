a138b252b35e77b77250dd36a3b20f82aebfb3c6
Carlos Henrique <ichfs9108@gmail.com>
2024-07-05 00:50:41 -0300

    Endpoint GET GetById

---
+++ PassIn/PassIn.Infrastructure/PassInDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Infrastructure.Entities;
+
+namespace PassIn.Infrastructure
+{
+    public class PassInDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=‪C:\\projetos\\PassInDb.db");
+        }
+    }
+}
